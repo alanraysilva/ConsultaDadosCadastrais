ceeb4e6e577bc7f86c4ec82f717f19209781d329
Alan Silva <alanraysilva@hotmail.com>
2021-03-23 14:02:48 -0300

    TESTE OK

--- ConsultaDadosCadastrais/Controllers/ConsultaController.cs
+++ ConsultaDadosCadastrais/Controllers/ConsultaController.cs
@@ -23,7 +23,7 @@ namespace ConsultaDadosCadastrais.Controllers
             ClaimsModel claimsModel = new ClaimsModel();
             claimsModel = Session["Claims"] as ClaimsModel;
             Session["nomeUsuario"] = claimsModel.UserName;
-            //TESTE
+            //OK
             return View();
                         
         }
